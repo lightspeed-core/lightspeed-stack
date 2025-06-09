d3d9d146508bec36a7f56b1ffb292f5fcc0a71f8
JR Boos <j.robert.boos@gmail.com>
2025-06-09 15:54:30 -0400

    Fixed Unit Test

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -10,13 +10,14 @@ from models.config import LLamaStackConfiguration
 
 # [tisnik] Need to resolve dependencies on CI to be able to run this tests
 @patch.dict(os.environ, {"INFERENCE_MODEL": "llama3.2:3b-instruct-fp16"})
-def _test_get_llama_stack_library_client() -> None:
+def test_get_llama_stack_library_client() -> None:
     cfg = LLamaStackConfiguration(
         url=None,
         api_key=None,
         use_as_library_client=True,
-        library_client_config_path="ollama",
+        library_client_config_path="./tests/configuration/minimal-stack.yaml",
     )
+
     client = get_llama_stack_client(cfg)
     assert client is not None
 
@@ -27,7 +28,7 @@ def test_get_llama_stack_remote_client() -> None:
         url="http://localhost:8321",
         api_key=None,
         use_as_library_client=False,
-        library_client_config_path="ollama",
+        library_client_config_path="./tests/configuration/minimal-stack.yaml",
     )
     client = get_llama_stack_client(cfg)
     assert client is not None
@@ -39,7 +40,7 @@ def test_get_llama_stack_wrong_configuration() -> None:
         url=None,
         api_key=None,
         use_as_library_client=True,
-        library_client_config_path="ollama",
+        library_client_config_path="./tests/configuration/minimal-stack.yaml",
     )
     cfg.library_client_config_path = None
     with pytest.raises(
